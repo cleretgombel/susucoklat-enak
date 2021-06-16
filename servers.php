35c3590185f20d7a09c3bf313bfc506e725666bf
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-16 22:39:02 +0700

    Create servers.php

---
+++ servers.php
@@ -0,0 +1,3 @@
+Selamat Datang Di Halaman Login Dan Info Dari New O.F.A..Untuk Acara Doa Bersama Rutin Di Adakan Setiap Malam Jumat Di mulai Setiap Jam 20:00 Wib Atau Jam 8 malam 21:00 Waktu Hongkong/Taiwan/Malaysia.. Terimaksih Telah Mengunakan Selalu Dan Bergabung Di New O.F.A..
+ofa.indonesia
+1003 1003 false X 3 -1
