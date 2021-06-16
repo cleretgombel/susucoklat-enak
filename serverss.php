5849133acc6e2a7bd49f6cc2badb1655bd4941a1
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-16 22:51:08 +0700

    Create serverss.php

---
+++ serverss.php
@@ -0,0 +1,45 @@
+TeamTalk 5 Official Server (UK)
+tt5eu.bearware.dk
+1033510335falseguestguest/TeamTalk 5 Official Server (US)
+tt5us.bearware.dk
+1033510335falsebearware/TeamTalk 5 Encrypted Server (UK)
+tt5eu.bearware.dk
+1044310443truebearware/Brazilian TeamTalk 5 Server (BR)
+androidparacegos.com.br
+1033310333false/Canadian TeamTalk 5 Server (CA)
+tt-ca.sytes.net
+1033510335falseguestguest/German TeamTalk 5 Server (DE)
+tt-german.duckdns.org
+1033510335falseguestguest/French TeamTalk 5 Server (FR)
+5.39.125.241
+1033310333falseguestguest/Georgian TeamTalk 5 Server (GE)
+tiflo.ge
+1033310333falseIndonesian TeamTalk Server (ID)
+smaci.my.id
+19591959falsetamutamu/Persian TeamTalk 5 Server (IR)
+5.56.132.178
+1030010300falsepublicpublicItalian TeamTalk 5 Server (IT)
+it.blind-access.com
+1033310333falsevisitatoreVisitatore123Malaysian TeamTalk 5 Server (MY)
+teamtalkmalaysia.my
+24502450falseguestEsperanto-Lando (NL)
+195.54.163.49
+1046010460false11CVLSB Public Server (NL)
+cvlsb.homedns.org
+1033510335falseguestguest/Polish TeamTalk 5 Server (PL)
+nvps.pl
+1033310333falsefacebookArabic TeamTalk 5 Server (PS)
+tt-ps.ddns.net
+1033310333falsepublicpublicRussian TeamTalk 5 Server (RU)
+prostochat.loveprod-host.ru
+1033310333falseInfiartt (SG)
+tt.infiartt.com
+19451945falseInfiarttKitaThai TeamTalk 5 Server (TH)
+netthai.thaiddns.com
+1033310333false/Turkish TeamTalk 5 Server (TR)
+2.59.117.217
+1033310333falsekonukkonuk/Audio Club (UA)
+audio-club.net
+1033410334falseUzbek TeamTalk 5 Server (UZ)
+shamscloud.uz
+1033310333falseGuestGuest
