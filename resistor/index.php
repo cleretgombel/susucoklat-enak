e67ef0e1a5467a64e293a8c8e5e0b65bb912d539
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2023-03-14 17:12:19 +0700

    Create index.php

---
+++ resistor/index.php
@@ -0,0 +1,2 @@
+<!DOCTYPE html>
+<br /><html><head></head><body><teamtalk version="5.0"><host><name>Republic of Intermerzo</name><address>147.139.168.73</address><tcpport>3534</tcpport><udpport>3534</udpport><encrypted>false</encrypted><auth><username>users</username><password>join</password></auth><join><channel>/</channel><password></password></join></host></teamtalk></body></html>
